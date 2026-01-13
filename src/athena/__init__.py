97ad8d20f7ecd8ff59f694c77d90386ea2a8df34
kevinchannon <kevinchannon@gmail.com>
2026-01-13 08:36:53 +0000

    Try to get version to actually report the version correctly

--- src/athena/__init__.py
+++ src/athena/__init__.py
@@ -1,3 +1,8 @@
 """Athena - A Python project."""
 
-__version__ = "0.1.0"
+try:
+    from importlib.metadata import version
+
+    __version__ = version("athena")
+except Exception:
+    __version__ = "0.0.0.dev0+local"  # Fallback for development
